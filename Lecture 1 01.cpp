1dbb727c4967adf3c77ded1fb980da8e807cfdaf
JaneWert1234 <113459839+JaneWert1234@users.noreply.github.com>
2022-09-15 19:56:01 +0300

    Add files via upload

---
+++ Lecture 1 01.cpp
@@ -0,0 +1,26 @@
+﻿
+
+#include <iostream>
+#include "my_max.h"
+#include <string>
+#include <complex>
+
+
+
+
+int main(int argc, char** argv)
+{
+    int i = 42;
+    std::cout << "max(7, i): " << ::max(7, i) << '\n';
+
+    double e = 2.71;
+    double pi = 3.14;
+    std::cout << "max(e, pi): " << ::max(e, pi) << '\n';
+
+    std::string s1 = "math";
+    std::string s2 = "mathematics";
+    std::cout << "max(s1, s2): " << ::max(s1, s2) << '\n';
+
+    //return 0;
+}
+
