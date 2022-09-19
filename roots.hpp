83fba8f3806bc48f9c5a7a154b9139410afe5402
JaneWert1234 <113459839+JaneWert1234@users.noreply.github.com>
2022-09-19 13:39:02 +0300

    Update roots.hpp

--- roots.hpp
+++ roots.hpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "sqrt.hpp"
 using namespace std;
 // шаблон функции вычисляющий корни квадратного уравнения
 
@@ -6,25 +7,25 @@ template<typename T>
 auto roots (T a, T b, T c) {
     T D = b*b - 4*a*c;
     if (a == 0 && b == 0 && c == 0) {
-        cout << "уравнение имеет бесконечное множество решений";
+        cout << "уравнение имеет бесконечное множество решений" << '\n';
     }
     else if (a == 0 && b == 0 && c != 0) {
-        cout << "корней нет";
+        cout << "корней нет" << '\n';
     }
     else if (a == 0 && b != 0) {
-        cout << "x = " << - c / b;
+        cout << "x = " << - c / b << '\n';
     }
     else if (D == 0) {
-        cout << "x = " << - b / (2*a);
+        cout << "x = " << - b / (2*a) << '\n';
     }
     else if (D > 0) {
-        cout << "x1 = " << (-b + ::sqrt(D))/(2*a) << ", x2 = " << (-b + ::sqrt(D))/(2*a);
+        cout << "x1 = " << (-b + ::sqrt(D))/(2*a) << ", x2 = " << (-b + ::sqrt(D))/(2*a) << '\n';
     }
     else if (D < 0) {
-        cout << "x1 = " << -b/(2*a) << "+" << ::sqrt(-D) << "i , x2 = " << -b/(2*a) << "-" << ::sqrt(-D) << "i";
+        cout << "x1 = " << -b/(2*a) << "+" << ::sqrt(-D) << "i , x2 = " << -b/(2*a) << "-" << ::sqrt(-D) << "i" << '\n';
     }
     else {
-        cout << "не верный ввод";
+        cout << "не верный ввод" << '\n';
     }
 
 
